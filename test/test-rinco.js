275b836240d5d042b8ee5db1a5f4f40a8eb77d43
Allan <loloudxur@gmail.com>
2015-07-17 21:17:30 -0300

    covering rinco fs tests

--- test/test-rinco.js
+++ test/test-rinco.js
@@ -1,8 +1,53 @@
-var assert = require('assert'),
-    rinco = require('./../rinco');
+var assert = require('assert');
+var rinco = require('./../lib/rinco');
+var should = require('should');
+var path = require('path');
 
 describe('Rinco', function () {
     it('should return false when no config file is found', function () {
-        assert.equal(rinco.checkConfigFile(), false);
+        assert.equal(rinco.util.hasConfig(), false);
     });
-})
+});
+
+// FS
+describe('fs.dir.write', function () {
+    it('should return true', function () {
+        should(rinco.fs.dir.write(path.join(__dirname, '/sandbox'))).be.exactly(true);
+    });
+});
+
+describe('fs.file.write', function () {
+    it('should fire done', function (done) {
+        should(rinco.fs.file.write(path.join(__dirname, '/sandbox', 'test.test'), 'testing', function () {
+            done();
+        }));
+    });
+});
+
+describe('fs.file.exist', function () {
+    it('should return true', function () {
+        should(rinco.fs.file.exist(path.join(__dirname, '/sandbox', 'test.test'))).be.exactly(true);
+    });
+});
+
+describe('fs.file.read', function () {
+    it('should return "testing"', function () {
+        should(rinco.fs.file.read(path.join(__dirname, '/sandbox', 'test.test')).toString()).be.exactly('testing');
+    });
+});
+
+describe('fs.dir.read', function () {
+    it('should fire done ', function (done) {
+        should(rinco.fs.dir.read(path.join(__dirname, '/sandbox'), function (filename) {
+           if(filename === 'test.test') {
+             done();
+           }
+        }));
+    });
+});
+
+describe('fs.dir.remove', function () {
+    it('should return true', function () {
+        should(rinco.fs.dir.remove(path.join(__dirname, '/sandbox'))).be.exactly(true);
+    });
+});
