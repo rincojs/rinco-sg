a0153f41a2428c552f2411da14a37d95dc5bebd1
Allan <loloudxur@gmail.com>
2015-07-17 23:36:14 -0300

    fixed prompt tasks

--- tasks.js
+++ tasks.js
@@ -8,32 +8,12 @@
 
 var rinco = require('./lib/rinco');
 
-// Registering tasks
-rinco.task.register("start-dev", function () {
-    rinco.server.start();
-    rinco.util.openSublime();
-});
 
 rinco.task.register("server", function () {
     rinco.server.start();
 });
 
-rinco.task.register("server:no-handlebars", function () {
-    rinco.server.start(["rinco_handlebars"]);
-});
 
 rinco.task.register("build", function () {
     rinco.util.build();
 });
-
-rinco.task.register("init", function () {
-    rinco.server.start(function (error) {
-        if (error) {
-            console.log('Invalid name!');
-            return false;
-        }
-        rinco.prompt.direction("list_tasks");
-    });
-});
-
-// rinco.task.register( "teste:grupo", ["teste"] );
