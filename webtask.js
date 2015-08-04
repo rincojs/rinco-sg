7359589c8bb2b986bca91c8564b5208c62ceb3b1
Allan <loloudxur@gmail.com>
2015-08-04 17:16:18 -0300

    webhook

--- webtask.js
+++ webtask.js
@@ -1,5 +1,5 @@
 module.exports = function(context, callback) {
   // context.webhook contains the webhook payload provided by GitHub
   // context.data contains URL query and webtask token parameters
-  callback(null, { some: 'result' });
+  callback(null, { some: context });
 }
