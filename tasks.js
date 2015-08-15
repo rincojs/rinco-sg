334d13f5c2d6343630436fca1be7618298e46f1f
allanesquina <loloudxur@gmail.com>
2015-08-15 00:29:38 -0300

    add library from cdnjs

--- tasks.js
+++ tasks.js
@@ -8,11 +8,17 @@
 
 var rinco = require('./lib/rinco');
 
-
 rinco.task.register("server", function () {
     rinco.server.start();
 });
 
+rinco.task.register("add", function (query) {
+    rinco.prompt.direction('list_search', query);
+});
+
+rinco.task.register("update", function (query) {
+    rinco.install.start();
+});
 
 rinco.task.register("build", function () {
     rinco.build.generate();
